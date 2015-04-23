6f2ccb261093f0e4cb47ee2b33652a974d48acd9
intchloe <chloe@cmail.nu>
2015-04-23 20:23:49 +0200

    Create tor.php

---
+++ tor.php
@@ -0,0 +1,13 @@
+<?php
+
+$url='https://onionoo.torproject.org/details?search=' . htmlspecialchars($_GET['s']);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
+$result=curl_exec($ch);
+curl_close($ch);
+$obj = (json_decode($result));
+foreach ($obj->relays AS $relay) printf("IP: %s | Country: %s | Name: %s | flags: %s | Consensus Weight: %s </br>", $relay->host_name, $relay->country_name, $relay->nickname, implode(',', $relay->flags), $relay->consensus_weight) . "
+\n";
+
+?>
